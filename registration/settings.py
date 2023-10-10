1372be9e8f7391906c718f26ebccbc7f94d9f455
Ramrekha yadav <94756876+Ramyadav08@users.noreply.github.com>
2023-10-10 21:42:23 +0530

    Update settings.py
    allowed

--- registration/settings.py
+++ registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p60+wny#o3#_*enai9-tl-d)6&w@!)95)#jzdvyn(s(0nme&-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
